d1d295a9431ce074bceb205ad3d8acdcba83c00e
AjayPath <ajaypath3@gmail.com>
2025-03-03 12:21:21 -0500

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,127 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final class NeoMotorConstants {
+    public static final double kFreeSpeedRpm = 5676;
+  }
+
+  public static class DriverControllerConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.05;
+  }
+
+  public static class OperatorControllerConstants {
+    public static final int kOperatorControllerPort = 1;
+  }
+
+  public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kSlowSpeedMetersPerSecond = 2;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(24.5);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(24.5);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0;
+    public static final double kBackLeftChassisAngularOffset = Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    // SPARK MAX CAN IDs
+    public static final int kFrontLeftDrivingCanId = 1;
+    public static final int kRearLeftDrivingCanId = 3;
+    public static final int kFrontRightDrivingCanId = 7;
+    public static final int kRearRightDrivingCanId = 5;
+
+    public static final int kFrontLeftTurningCanId = 2;
+    public static final int kRearLeftTurningCanId = 4;
+    public static final int kFrontRightTurningCanId = 8;
+    public static final int kRearRightTurningCanId = 6;
+
+    public static final boolean kGyroReversed = false;
+  }
+
+  public static final class ModuleConstants {
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+    // more teeth will result in a robot that drives faster).
+    public static final int kDrivingMotorPinionTeeth = 13;
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
+    public static final double kWheelDiameterMeters = 0.0762;
+    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
+    public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+    public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+        / kDrivingMotorReduction;
+  }
+
+  public static final class ElevatorConstants {
+
+    public static final int kElevator1CanId = 50;
+    public static final int kElevator2CanId = 51;
+
+    public static final double kFeederStation = 20; //14
+    public static final double kTravel = 0;
+    public static final double kLevel1 = 0;
+    public static final double kLevel2 = 19; //16
+    public static final double kLevel3 = 8; // 8
+    public static final double kLevel4 = 40; //88
+    public static final double kHome = 0;
+
+  }
+
+  public static final class ArmConstants {
+
+    public static final int kArmCanId = 55;
+
+    public static final double kFeederStation = -6; // -4
+    public static final double kTravel = 98;
+    public static final double kLevel1 = 0;
+    public static final double kLevel2 = 0; //-7
+    public static final double kLevel3 = 87;
+    public static final double kLevel4 = 82; //84
+    public static final double kHome = 0; //-10
+
+  }
+
+  public static final class CoralIntakeConstants {
+
+    public static final int kCoralIntakeCanId = 60;
+
+    public static final double kCoralIntakeSpeed = 0.25;
+    public static final double kCoralOutakeSpeed = -1;
+
+  }
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,175 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.CoralIntakeConstants;
+import frc.robot.Constants.DriverControllerConstants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.OperatorControllerConstants;
+import frc.robot.commands.CoralIntakeCmd;
+import frc.robot.commands.MoveArmToSetpoint;
+import frc.robot.commands.MoveElevatorToSetpoint;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.CoralIntakeSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+import java.util.Map;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DriveSubsystem s_driveSubsystem = new DriveSubsystem();
+  private final CoralIntakeSubsystem s_CoralIntakeSubsystem = new CoralIntakeSubsystem();
+  private final ArmSubsystem s_ArmSubsystem = new ArmSubsystem();
+  private final ElevatorSubsystem s_ElevatorSubsystem = new ElevatorSubsystem();
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(DriverControllerConstants.kDriverControllerPort);
+
+  private final CommandXboxController m_operatorController = 
+      new CommandXboxController(OperatorControllerConstants.kOperatorControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+    s_driveSubsystem.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> s_driveSubsystem.drive(
+                -MathUtil.applyDeadband(m_driverController.getLeftY(), DriverControllerConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), DriverControllerConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), DriverControllerConstants.kDriveDeadband),
+                true),
+            s_driveSubsystem));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+   
+    // CORAL INTAKE BUTTONS
+
+    m_driverController.leftBumper().whileTrue(
+      new CoralIntakeCmd(s_CoralIntakeSubsystem, 1)
+    );
+
+    m_driverController.leftBumper().whileFalse(
+      new CoralIntakeCmd(s_CoralIntakeSubsystem, 0)
+    );
+
+    m_driverController.rightBumper().whileTrue(
+    new CoralIntakeCmd(s_CoralIntakeSubsystem, -1)
+    );
+
+    m_driverController.rightBumper().whileFalse(
+      new CoralIntakeCmd(s_CoralIntakeSubsystem, 0)
+    );
+
+    // Level 4
+
+    m_operatorController.x().onTrue(
+
+      //new SequentialCommandGroup(
+        //new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kLevel4),
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kLevel4)
+      //)
+
+    );
+
+    // Feeder Position
+
+    m_operatorController.a().onTrue(
+
+      new SequentialCommandGroup(
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kFeederStation),new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kFeederStation)
+      )
+
+    );
+
+    m_operatorController.y().onTrue(
+
+      new SequentialCommandGroup(
+        new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kLevel3),
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kLevel3)
+      )
+
+    );
+
+    m_operatorController.b().onTrue(
+
+      new SequentialCommandGroup(
+        new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kLevel2),
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kHome)
+      )
+
+    );
+
+    m_operatorController.leftBumper().onTrue(
+      new SequentialCommandGroup(
+        new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kTravel),
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kTravel)
+      )
+    );
+
+    m_operatorController.rightBumper().onTrue(
+      //new SequentialCommandGroup(
+        new MoveElevatorToSetpoint(s_ElevatorSubsystem, ElevatorConstants.kHome)
+          //new MoveArmToSetpoint(s_ArmSubsystem, ArmConstants.kHome)
+        //)
+    );
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return null;
+  }
+
+}
